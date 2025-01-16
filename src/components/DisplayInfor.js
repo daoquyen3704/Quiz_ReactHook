e42f0c5ea4f7af64ca60f20b94e548aa2a7584df
Dao Duy Quyen <quyenne3704@gmail.com>
2025-01-16 22:35:24 +0700

    stateless & stateful

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,59 +1,68 @@
 import React from "react";
 import './DisplayInfor.scss';
-class DisplayInfor extends React.Component {
-    state = {
-        isShowListUser: true
-    }
-    handleShowHide = () => {
-        this.setState({
-            isShowListUser: !this.state.isShowListUser
-        })
-    }
-    componentDidMount() {
-        console.log('call me component did mount')
-        setTimeout(() => {
-            document.title = 'Eric'
-        }, 3000)
-    }
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('>>> call me component update', this.props, prevProps)
-        if (this.props.listUsers !== prevProps.listUsers) {
-            if (this.props.listUsers.length === 5) {
-                alert('You got 5 users')
-            }
-        }
-    }
-    render() {
-        // destructuring array/obj
-        const { listUsers } = this.props;
-        return (
-            <div className="display-infor-container">
-                <div>
-                    <span onClick={() => this.handleShowHide()}>
-                        {this.state.isShowListUser ? "Hide List User" : "Show List User"}
-                    </span>
-                </div>
-                {this.state.isShowListUser &&
-                    <>
-                        {listUsers.map((user, index) => {
-                            return (
-                                <>
-                                    <div key={user.id} className={+user.age > 18 ? "blue" : "red"}>
-                                        <div>My Name's {user.name}</div>
-                                        <div>My Age's {user.age}</div>
 
-                                    </div>
-                                    <div>
-                                        <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
-                                    </div>
-                                    <hr />
-                                </>
-                            )
-                        })}
-                    </>
-                }
-            </div>
-        )
-    }
+//stateless vs stateful
+// class DisplayInfor extends React.Component {
+//     state = {
+//         isShowListUser: true
+//     }
+
+
+//     render() {
+//         // destructuring array/obj
+//         const { listUsers } = this.props;
+//         return (
+//             <div className="display-infor-container">
+
+//                 {true &&
+//                     <>
+//                         {listUsers.map((user, index) => {
+//                             return (
+//                                 <>
+//                                     <div key={user.id} className={+user.age > 18 ? "blue" : "red"}>
+//                                         <div>My Name's {user.name}</div>
+//                                         <div>My Age's {user.age}</div>
+
+//                                     </div>
+//                                     <div>
+//                                         <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
+//                                     </div>
+//                                     <hr />
+//                                 </>
+//                             )
+//                         })}
+//                     </>
+//                 }
+//             </div>
+//         )
+//     }
+// }
+
+const DisplayInfor = (props) => {
+    const { listUsers } = props;
+    return (
+        <div className="display-infor-container">
+
+            {true &&
+                <>
+                    {listUsers.map((user, index) => {
+                        return (
+                            <>
+                                <div key={user.id} className={+user.age > 18 ? "blue" : "red"}>
+                                    <div>My Name's {user.name}</div>
+                                    <div>My Age's {user.age}</div>
+
+                                </div>
+                                <div>
+                                    <button onClick={() => props.handleDeleteUser(user.id)}>Delete</button>
+                                </div>
+                                <hr />
+                            </>
+                        )
+                    })}
+                </>
+            }
+        </div>
+    )
 }
 export default DisplayInfor;
