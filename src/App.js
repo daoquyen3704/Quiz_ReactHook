2d578faa49160b5db74150f428c4fbe21cd05918
Dao Duy Quyen <quyenne3704@gmail.com>
2025-01-23 23:47:21 +0700

    Site navigation with Links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <div className="app-container">
       <Header />
+      <div>
+        Test Link
+        <div>
+          <button>
+            <Link to={`/users`}>go to user page</Link>
+          </button>
+
+          <button>
+            <Link to={`/admins`}>go to admin page</Link>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
