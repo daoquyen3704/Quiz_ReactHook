0a21371fc351c27bdc16f3e83f07d6b5f8ae7028
Dao Duy Quyen <quyenne3704@gmail.com>
2025-01-24 22:48:37 +0700

    index routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,8 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Hỏi Dân IT</Navbar.Brand>
+                {/* <Navbar.Brand href="#home">Hỏi Dân IT</Navbar.Brand> */}
+                <NavLink to={'/'} className='navbar-brand'>Hỏi Dân IT</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -23,11 +24,9 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Settings" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Log Out
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+                            <NavDropdown.Item >Log in</NavDropdown.Item>
+                            <NavDropdown.Item >Log Out</NavDropdown.Item>
+                            <NavDropdown.Item >Profile</NavDropdown.Item>
 
                         </NavDropdown>
                     </Nav>
