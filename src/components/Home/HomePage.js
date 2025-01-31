58ff60339d0a4eb9026a6a8ffa88b67954d4ca8a
Dao Duy Quyen <quyenne3704@gmail.com>
2025-01-31 11:06:10 +0700

    design homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,10 @@
+import videoHomePage from '../../assets/video-homepage.mp4';
 const HomePage = (props) => {
     return (
         <div className="homepage-container">
-            homepage component
+            <video autoPlay muted loop>
+                <source src={videoHomePage} type = "video/mp4" />
+                </video>
         </div>
     )
 }
