4afd71bfad87a2eb837a271182dfa674aefd8b9f
Dao Duy Quyen <quyenne3704@gmail.com>
2025-02-12 22:53:58 +0700

    Update table users when create sucessfully

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -3,10 +3,25 @@ import './ManageUser.scss';
 import { FcPlus } from "react-icons/fc";
 import { useState } from "react";
 import TableUsers from "./TableUsers";
-
+import { useEffect } from "react";
+import { getAllUsers } from "../../../services/apiService";
 
 const ManageUser = (props) => {
     const [showModalCreateUser, setShowModalCreateUser] = useState(false);
+    const [listUsers, setListUsers] = useState([])
+
+    useEffect(() => {
+        fetchListUsers();
+    }, []);
+    const fetchListUsers = async () => {
+        let res = await getAllUsers();
+        // console.log(res);
+        if (res.EC === 0) {
+            setListUsers(res.DT)
+        }
+    }
+
+
     return (
         <div className="manage-user-container">
             <div className="title">
@@ -19,11 +34,12 @@ const ManageUser = (props) => {
                     </button>
                 </div>
                 <div className="table-user-container">
-                    <TableUsers />
+                    <TableUsers listUsers={listUsers} />
                 </div>
                 <ModalCreateUser
                     show={showModalCreateUser}
                     setShow={setShowModalCreateUser}
+                    fetchListUsers={fetchListUsers}
                 />
             </div>
         </div>
