3e697b2786db56641afc0492206f3b5169f0d755
Dao Duy Quyen <quyenne3704@gmail.com>
2025-02-24 22:24:21 +0700

    use Redux for save user infor

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,15 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { NavLink, useNavigate } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+    const isAuthenticated = useSelector(state => state.user.isAuthenticated);
+    const account = useSelector(state => state.user.account);
     const navigate = useNavigate();
     const handleLogin = () => {
         navigate('/login');
+        console.log('authen', isAuthenticated)
     }
     const handleRegister = () => {
         navigate('/register');
@@ -30,14 +33,18 @@ const Header = () => {
 
                     </Nav>
                     <Nav>
-                        <button className='btn-login' onClick={() => handleLogin()}>Log in</button>
-                        <button className='btn-signup' onClick={() => handleRegister()}>Sign up</button>
-                        {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
-                            <NavDropdown.Item >Log in</NavDropdown.Item>
-                            <NavDropdown.Item >Log Out</NavDropdown.Item>
-                            <NavDropdown.Item >Profile</NavDropdown.Item>
+                        {isAuthenticated === false ?
 
-                        </NavDropdown> */}
+                            <>
+                                <button className='btn-login' onClick={() => handleLogin()}>Log in</button>
+                                <button className='btn-signup' onClick={() => handleRegister()}>Sign up</button>
+                            </>
+                            :
+                            <NavDropdown title="Settings" id="basic-nav-dropdown">
+                                <NavDropdown.Item >Log Out</NavDropdown.Item>
+                                <NavDropdown.Item >Profile</NavDropdown.Item>
+                            </NavDropdown>
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,9 +1,14 @@
 import videoHomePage from '../../assets/video-homepage.mp4';
+import { useSelector } from 'react-redux';
+
 const HomePage = (props) => {
+    const isAuthenticated = useSelector(state => state.user.isAuthenticated);
+    const account = useSelector(state => state.user.account);
+    // console.log('account: ', account, ' isAuthenticated: ', isAuthenticated);
     return (
         <div className="homepage-container">
             <video autoPlay muted loop>
-                <source src={videoHomePage} type = "video/mp4" />
+                <source src={videoHomePage} type="video/mp4" />
             </video>
             <div className='homepage-content'>
                 <div className='title-1'>
