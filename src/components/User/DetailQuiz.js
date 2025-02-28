ac043acadf52a6433f5eeca34d9b0dbe071f634b
Dao Duy Quyen <quyenne3704@gmail.com>
2025-02-28 16:47:23 +0700

    Process Data

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
-
+import _ from "lodash";
 
 const DetailQuiz = (props) => {
     const params = useParams();
@@ -14,7 +14,26 @@ const DetailQuiz = (props) => {
     const fetchQuestion = async () => {
         const res = await getDataQuiz(quizId);
         console.log('check question: ', res)
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                .groupBy("id")
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDescription, image = null;
 
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answers);
+                    })
+                    return { questionId: key, answers, questionDescription, image }
+                })
+                .value()
+            console.log(data)
+        }
     }
     return (
         <div className="detail-quiz-container">
