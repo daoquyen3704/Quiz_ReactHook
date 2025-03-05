420cb294a70cb9afd2f3c26d077e96156e7dd30e
Dao Duy Quyen <quyenne3704@gmail.com>
2025-03-06 00:19:22 +0700

    display list quiz

--- src/components/Admin/Content/Quiz/ManageQuiz.js
+++ src/components/Admin/Content/Quiz/ManageQuiz.js
@@ -5,7 +5,8 @@ import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import { postCreateNewQuiz } from '../../../../services/apiService';
 import { toast } from 'react-toastify';
-
+import QuizTable from './QuizTable';
+import Accordion from 'react-bootstrap/Accordion';
 
 
 
@@ -54,73 +55,76 @@ const ManageQuiz = (props) => {
     }
     return (
         <div className="quiz-container">
-            <div className="title">
-                Manage Quizzes
-            </div>
-            <hr />
-            <div className="add-new">
-                <fieldset className="border rounded-3 p-3">
-                    <legend className="float-none w-auto px-3">Add New Quiz:</legend>
-                    <div className="form-floating mb-3">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder='your quiz name'
-                            value={name}
-                            onChange={(event) => setName(event.target.value)}
-                        />
-                        <label>Name</label>
-                    </div>
-                    <div className="form-floating">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder='description'
-                            value={description}
-                            onChange={(event) => setDescription(event.target.value)}
-                        />
-                        <label>Description</label>
-                    </div>
-                    <div className='my-3'>
-                        <Select
-                            defaultValue={type}
-                            onChange={setType}
-                            options={options}
-                            placeholder={"Quiz type..."}
-                        />
-                    </div>
-                    <div className='more-actions'>
-                        <div className='col-md-12'>
-                            <label className='mb-1 form-label label-upload' htmlFor='labelUpload'>
-                                <FcPlus />
-                                Upload File Image
-                            </label>
-                            <input
-                                type='file'
-                                id='labelUpload'
-                                hidden
-                                onChange={(event) => handleUploadImage(event)}
-                                className='form-control'
-                            />
+            <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Manage Quizzes</Accordion.Header>
+                    <Accordion.Body>
+                        <div className="add-new">
+                            <fieldset className="border rounded-3 p-3">
+                                <legend className="float-none w-auto px-3">Add New Quiz:</legend>
+                                <div className="form-floating mb-3">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder='your quiz name'
+                                        value={name}
+                                        onChange={(event) => setName(event.target.value)}
+                                    />
+                                    <label>Name</label>
+                                </div>
+                                <div className="form-floating">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder='description'
+                                        value={description}
+                                        onChange={(event) => setDescription(event.target.value)}
+                                    />
+                                    <label>Description</label>
+                                </div>
+                                <div className='my-3'>
+                                    <Select
+                                        defaultValue={type}
+                                        onChange={setType}
+                                        options={options}
+                                        placeholder={"Quiz type..."}
+                                    />
+                                </div>
+                                <div className='more-actions'>
+                                    <div className='col-md-12'>
+                                        <label className='mb-1 form-label label-upload' htmlFor='labelUpload'>
+                                            <FcPlus />
+                                            Upload File Image
+                                        </label>
+                                        <input
+                                            type='file'
+                                            id='labelUpload'
+                                            hidden
+                                            onChange={(event) => handleUploadImage(event)}
+                                            className='form-control'
+                                        />
+                                    </div>
+                                    <div className='col-md-12 image-preview'>
+                                        {previewImage ?
+                                            <img src={previewImage} />
+                                            :
+                                            <span>Preview Image</span>
+                                        }
+                                    </div>
+                                </div>
+                                <div className='mt-3'>
+                                    <button
+                                        className='btn btn-warning'
+                                        onClick={() => handleSubmitQuiz()}
+                                    >Save</button>
+                                </div>
+                            </fieldset>
                         </div>
-                        <div className='col-md-12 image-preview'>
-                            {previewImage ?
-                                <img src={previewImage} />
-                                :
-                                <span>Preview Image</span>
-                            }
-                        </div>
-                    </div>
-                    <div className='mt-3'>
-                        <button
-                            className='btn btn-warning'
-                            onClick={() => handleSubmitQuiz()}
-                        >Save</button>
-                    </div>
-                </fieldset>
-            </div>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
             <div className="list-detail">
-                table
+                <QuizTable />
             </div>
         </div>
     )
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -67,8 +67,11 @@ const postCreateNewQuiz = (description, name, difficulty, image) => {
     return axios.post('api/v1/quiz', data);
 }
 
+const getAllQuizForAdmin = () => {
+    return axios.get(`api/v1/quiz/all`);
+}
 export {
     postCreateNewUser, getAllUsers, putUpdateUser, deleteUser,
     getUserWithPaginate, postLogin, postRegister, getQuizByUser, getDataQuiz,
-    postSubmitQuiz, postCreateNewQuiz
+    postSubmitQuiz, postCreateNewQuiz, getAllQuizForAdmin
 }
